10f0c1f387b30fca78804531f9834e9f961897a9
Andrew Nichols <andrew@frenata.net>
2025-01-30 17:18:54 +0100

    get interactive going

--- chat.py
+++ chat.py
@@ -1,6 +1,8 @@
+import collections
 import argparse
-from ollama import chat
+from ollama import chat, Message
 from rich import print
+from rich.prompt import Prompt
 from rich.columns import Columns
 from rich.panel import Panel
 from rich.console import Console
@@ -14,9 +16,7 @@ def parse_models():
         help="One or more model names to send the message to",
         required=True,
     )
-    parser.add_argument(
-        "--message", required=True, help="The message to send to the models"
-    )
+    parser.add_argument("--message", help="The message to send to the models")
     args = parser.parse_args()
     return args.model, args.message
 
@@ -24,6 +24,17 @@ def parse_models():
 def print_columns(responses):
     console = Console()
     responses = [Panel(res[1], title=res[0]) for res in responses]
+    cols = Columns(
+        responses, equal=True, width=(console.size.width // len(responses) - 2)
+    )
+    print(cols)
+
+
+def print_chats(chats):
+    console = Console()
+    responses = [
+        Panel(messages[-1].content, title=model) for model, messages in chats.items()
+    ]
     cols = Columns(
         responses, equal=True, width=(console.size.width // len(responses) - 2)
     )
@@ -44,6 +55,24 @@ def main(models, message):
     print_columns(responses)
 
 
+def interactive(models):
+    messages = collections.defaultdict(list)
+
+    while True:
+        responses = []
+        message = str(Prompt.ask(">> "))
+        for model in models:
+            user_message = Message(role="user", content=message)
+            messages[model].append(user_message)
+            model_message: dict = chat(model=model, messages=messages[model])
+            messages[model].append(model_message.message)
+            responses.append((model, model_message.message.content))
+        print_chats(messages)
+
+
 if __name__ == "__main__":
     models, message = parse_models()
-    main(models, message)
+    if message is not None:
+        main(models, message)
+    else:
+        interactive(models)
